b05e3b67a9fe7bf488d3d7844a30d805a7b821ac
Zhu Weiji <zhuweiji1997@gmail.com>
2023-04-10 18:44:51 +0800

    feature: imagerank end to end (added the scoring and ranking of images)

--- cloud-worker/cloud_worker/services/task_processer.py
+++ cloud-worker/cloud_worker/services/task_processer.py
@@ -17,6 +17,7 @@ class TaskType(Enum):
     KEYWORD_EXTRACTION  = 'KEYWORD_EXTRACTION'
     IMAGE_TRANSCRIPTION = 'IMAGE_TRANSCRIPTION'
     SENTENCE_EXTRACTION = 'SENTENCE_EXTRACTION'
+    IMAGE_RANK          = 'IMAGE_RANK'
 
 class TaskProcesor:
     work_queue_provider = RabbitMQHandler
@@ -28,12 +29,13 @@ class TaskProcesor:
         
         task_type = headers['task_type']
         task_id = headers['task_id']
+        pickled = headers.get('pickled',None)
         other_info:dict = headers['other_info'] # type: ignore
         
         log.info(f'processing new {task_type} task')
         
         if task_type == TaskType.KEYWORD_EXTRACTION.value:
-            data = message.body.decode()
+            data = message.body.decode() if not pickled else pickle.loads(message.body)
             
             result =  cls.job_handler.keyword_extraction__undirected(data)
             encoded_data = pickle.dumps(result)
@@ -57,7 +59,16 @@ class TaskProcesor:
             
         elif task_type == TaskType.SENTENCE_EXTRACTION.value:
             data = message.body.decode()
+            result = cls.job_handler.sentence_extraction__undirected(data)
+            encoded_data = pickle.dumps(result)
             
+            await RabbitMQHandler.publish(RABBITMQ_RESULT_QUEUE_NAME, encoded_data, 
+                                          {'task_type': TaskType.SENTENCE_EXTRACTION.value,
+                                           'task_id':task_id,
+                                           'pickled': True})
+        elif task_type == TaskType.IMAGE_RANK.value:
+            data = message.body.decode()
+            data = data.split('|')
             result = cls.job_handler.sentence_extraction__undirected(data)
             encoded_data = pickle.dumps(result)
             
@@ -69,7 +80,11 @@ class TaskProcesor:
         else:
             log.warning(f'Could not interpret task: {headers}')
             
+        
+            
         log.info(f'completed {task_type} task')
+        
+        
             
     @classmethod
     async def listen_for_incoming_tasks(cls):
